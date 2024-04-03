793cbef0b9a6a5dbc2dee7fdd40935d6b0a5af41
Jleo <kamnevvova@gmail.com>
2024-04-03 18:36:22 +0300

    update

--- scripts/menu/menu.js
+++ scripts/menu/menu.js
@@ -18,3 +18,22 @@ const createMenuBlock = (height, width) => {
 }
 
 createMenuBlock(1000, 2000);
+
+const playAudio = async (path) => {
+    return new Promise((resolve, reject) => {
+        const audio = new Audio();
+        audio.preload = "auto";
+        audio.autoplay = true;
+        audio.onerror = reject;
+        audio.onended = resolve;
+        audio.src = path;
+    })
+}
+
+const button = document.getElementById("button")
+button.addEventListener("click", (event) => {
+    event.preventDefault()
+    playAudio("./sounds/minecraft_click.mp3").then(() => window.location.href = "game.html")
+})
+
+setTimeout(async () => {await playAudio("./music/C418_Key.mp3")}, 1000)
